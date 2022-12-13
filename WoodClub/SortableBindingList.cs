fb886f3d0b6828a11a2d4243831a8b23cd61ca43
Ed Sproull <ecs3@po.cwru.edu>
2022-12-13 12:43:43 -0700

    Revised columns in the main view

--- WoodClub/SortableBindingList.cs
+++ WoodClub/SortableBindingList.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.ComponentModel;
-using WoodClub;
 
 /// <summary>
 /// Original source
@@ -12,13 +8,13 @@ using WoodClub;
 /// </summary>
 namespace WoodClub
 {
-    /// <summary>
-    /// Provides a generic collection that supports data binding and additionally supports sorting.
-    /// See http://msdn.microsoft.com/en-us/library/ms993236.aspx
-    /// If the elements are IComparable it uses that; otherwise compares the ToString()
-    /// </summary>
-    /// <typeparam name="T">The type of elements in the list.</typeparam>
-    public class SortableBindingList<T> : BindingList<T> where T : class
+	/// <summary>
+	/// Provides a generic collection that supports data binding and additionally supports sorting.
+	/// See http://msdn.microsoft.com/en-us/library/ms993236.aspx
+	/// If the elements are IComparable it uses that; otherwise compares the ToString()
+	/// </summary>
+	/// <typeparam name="T">The type of elements in the list.</typeparam>
+	public class SortableBindingList<T> : BindingList<T> where T : class
     {
         private bool _isSorted;
         private ListSortDirection _sortDirection = ListSortDirection.Ascending;
@@ -148,7 +144,12 @@ namespace WoodClub
             return lhsValue.ToString().CompareTo(rhsValue.ToString());
         }
         public static bool IsNumeric(object value)
-        { 
+        {
+            if (value.ToString() == "True" || value.ToString() == "False")
+            {
+                return false;
+            }
+
             int i;
             return Int32.TryParse((string)value, out i);                   
         }
