1aebc8f1d07c2893fb06db14123b7fa2db316597
ecsproull <ecsproull@outlook.com>
2024-09-23 14:53:32 -0700

    Ready to add locker, shop usage email

--- WoodClub/Properties/Settings.Designer.cs
+++ WoodClub/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WoodClub {
+namespace WoodClub.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
