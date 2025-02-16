e44b6d1275643adfff3b102706055e1c6462be42
ecsproull <ecsproull@outlook.com>
2025-02-16 10:28:34 -0700

    Added a Machine Permissions Report

--- WoodClub/Forms/PermissionsReport.Designer.cs
+++ WoodClub/Forms/PermissionsReport.Designer.cs
@@ -29,22 +29,64 @@
 		private void InitializeComponent()
 		{
 			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.excel = new System.Windows.Forms.Button();
+			this.print = new System.Windows.Forms.Button();
+			this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+			this.email_list = new System.Windows.Forms.Button();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// dataGridView1
 			// 
+			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			this.dataGridView1.Location = new System.Drawing.Point(3, 63);
+			this.dataGridView1.Location = new System.Drawing.Point(3, 36);
 			this.dataGridView1.Name = "dataGridView1";
-			this.dataGridView1.Size = new System.Drawing.Size(793, 351);
+			this.dataGridView1.Size = new System.Drawing.Size(793, 378);
 			this.dataGridView1.TabIndex = 0;
+			// 
+			// excel
+			// 
+			this.excel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.excel.Location = new System.Drawing.Point(240, 422);
+			this.excel.Name = "excel";
+			this.excel.Size = new System.Drawing.Size(86, 23);
+			this.excel.TabIndex = 1;
+			this.excel.Text = "Excel";
+			this.excel.UseVisualStyleBackColor = true;
+			this.excel.Click += new System.EventHandler(this.excel_Click);
+			// 
+			// print
+			// 
+			this.print.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.print.Location = new System.Drawing.Point(485, 424);
+			this.print.Name = "print";
+			this.print.Size = new System.Drawing.Size(75, 20);
+			this.print.TabIndex = 2;
+			this.print.Text = "Print";
+			this.print.UseVisualStyleBackColor = true;
+			this.print.Click += new System.EventHandler(this.print_Click);
+			// 
+			// email_list
+			// 
+			this.email_list.Location = new System.Drawing.Point(374, 422);
+			this.email_list.Name = "email_list";
+			this.email_list.Size = new System.Drawing.Size(66, 21);
+			this.email_list.TabIndex = 3;
+			this.email_list.Text = "Email List";
+			this.email_list.UseVisualStyleBackColor = true;
+			this.email_list.Click += new System.EventHandler(this.email_list_Click);
 			// 
 			// PermissionsReport
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.email_list);
+			this.Controls.Add(this.print);
+			this.Controls.Add(this.excel);
 			this.Controls.Add(this.dataGridView1);
 			this.Name = "PermissionsReport";
 			this.Text = "PermissionsReport";
@@ -57,5 +99,9 @@
 		#endregion
 
 		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Button excel;
+		private System.Windows.Forms.Button print;
+		private System.Drawing.Printing.PrintDocument printDocument1;
+		private System.Windows.Forms.Button email_list;
 	}
 }
--- WoodClub/Forms/PermissionsReport.cs
+++ WoodClub/Forms/PermissionsReport.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,19 @@ namespace WoodClub.Forms
 {
 	public partial class PermissionsReport : Form
 	{
+		private List<string> filters = new List<string>();
+		private SortableBindingList<PermissionReportItem> bindingList;
 		public PermissionsReport()
 		{
 			InitializeComponent();
+			printDocument1.PrintPage += PrintDocument1_PrintPage;
+		}
+
+		private void PrintDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+		{
+			Bitmap bm = new Bitmap(this.dataGridView1.Width, this.dataGridView1.Height);
+			this.dataGridView1.DrawToBitmap(bm, new Rectangle(0, 0, this.dataGridView1.Width, this.dataGridView1.Height));
+			e.Graphics.DrawImage(bm, 0, 0);
 		}
 
 		private void PermissionsReport_Load(object sender, EventArgs e)
@@ -36,8 +47,8 @@ namespace WoodClub.Forms
 					box.Text = p.ToString().ToUpper();
 					box.Height = 20;
 					box.Width = 95;
-					box.Location = new Point(i++ * 100 + 15, 10); //vertical
-															   //box.Location = new Point(i * 50, 10); //horizontal
+					box.Location = new Point(i++ * 100 + 15, 10);
+					box.Click += new EventHandler(checkbox_Click);
 					this.Controls.Add(box);
 				}
 
@@ -58,8 +69,124 @@ namespace WoodClub.Forms
 				.Where(p => p.Permission != "lbc")
 				.OrderBy(x => x.Badge).ToList();
 
-				dataGridView1.DataSource = new SortableBindingList<PermissionReportItem>(permList);
+				bindingList = new SortableBindingList<PermissionReportItem>(permList);
+				dataGridView1.DataSource = bindingList;
+				dataGridView1.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
 			}
+		}
+
+		void checkbox_Click(object sender, EventArgs e)
+		{
+			CheckBox box = ((CheckBox)sender);
+			string text = box.Text.Trim().ToLower();
+			if (box.Checked)
+			{
+				filters.Add(text);
+			}
+			else
+			{
+				filters.Remove(text);
+			}
+
+
+
+			//if list is empty clear filters.
+			//else filter and refresh.
+			string filter = string.Empty;
+			foreach (var filterItem in filters)
+			{
+				if (string.IsNullOrEmpty(filter))
+				{
+					filter = filterItem;
+				}
+				else
+				{
+					filter += " || " + filterItem;
+				}
+			}
+
+			if (filter == string.Empty)
+			{
+				dataGridView1.DataSource = bindingList;
+			}
+			else
+			{
+				SortableBindingList< PermissionReportItem> filteredBindingList = 
+					new SortableBindingList<PermissionReportItem>(bindingList.Where(
+					x => filters.Contains(x.Permission)).ToList());
+				dataGridView1.DataSource = filteredBindingList;
+				dataGridView1.Refresh();
+			}
+		}
+
+		private void excel_Click(object sender, EventArgs e)
+		{
+			string pathDesktop = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%") + "\\Documents";
+			string delimter = ",";
+			SaveFileDialog saveFileDialog = new SaveFileDialog();
+			saveFileDialog.Title = "Save CSV File";
+			saveFileDialog.FileName = "permissions.csv";
+			saveFileDialog.InitialDirectory = saveFileDialog.InitialDirectory = Environment.ExpandEnvironmentVariables("%HOMEDRIVE%%HOMEPATH%") + "\\Documents";
+			saveFileDialog.Filter = "CSV files|*.csv";
+			if (saveFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				string filePath = saveFileDialog.FileName;
+				try
+				{
+					SortableBindingList<PermissionReportItem> currentList = (SortableBindingList <PermissionReportItem>)dataGridView1.DataSource;
+					int length = currentList.Count();
+					string hdr = "Badge,Permission,First,Last,Email,Phone";
+
+					using (System.IO.TextWriter writer = File.CreateText(filePath))
+					{
+						writer.WriteLine(hdr);
+						for (int index = 0; index < length; index++)
+						{
+							PermissionReportItem item = currentList[index];
+							string csv = item.Badge + "," +
+										 item.Permission + "," +
+										 item.FirstName + "," +
+										 item.LastName + "," +
+										 item.Email + "," +
+										 item.Phone;
+							writer.WriteLine(string.Join(delimter, csv));
+						}
+					}
+
+					System.Diagnostics.Process.Start(filePath);
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show("Error: Could not write file to disk. Original error: " + ex.Message);
+				}
+			}
+		}
+
+		private void print_Click(object sender, EventArgs e)
+		{
+			printDocument1.DefaultPageSettings.Landscape = true;
+			PrintPreviewDialog ppvd = new PrintPreviewDialog();
+			ppvd.Document = printDocument1;
+			ppvd.ShowDialog();
+		}
+
+		private void email_list_Click(object sender, EventArgs e)
+		{
+			string emailAddresses = string.Empty;
+			SortableBindingList<PermissionReportItem> currentList = (SortableBindingList<PermissionReportItem>)dataGridView1.DataSource;
+			foreach (PermissionReportItem item in currentList)
+			{
+				if (string.IsNullOrEmpty(emailAddresses))
+				{
+					emailAddresses = item.Email;
+				}
+				else
+				{
+					emailAddresses += ';' + item.Email;
+				}
+			}
+			System.Windows.Forms.Clipboard.SetText(emailAddresses);
+			MessageBox.Show("Email Address Copied to Clipboard");
 		}
 	}
 }
